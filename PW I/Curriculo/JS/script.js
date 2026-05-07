59ea4f898d4454217f4e14718317b3a97c6d8174
otavionavarro <otavionavarro2011@gmail.com>
2026-05-07 13:48:36 -0300

    Add files via upload

--- PW I/Curriculo/JS/script.js
+++ PW I/Curriculo/JS/script.js
@@ -1,15 +1,17 @@
 const botao = document.getElementById("Tema");
 
 // carregar prefêrencia
-if (LocalStorage.getItem("tema") === "dark") {
+if (localStorage.getItem("tema") === "dark") {
     document.body.classList.add("dark");
 }
 
 // alternar tema
-botao.addEventListener("click", () => {
+botao.addEventListener("click",() => {
+    document.body.classList.toggle("dark");
+
     if (document.body.classList.contains("dark")) {
-        LocalStorage.setItem("tema", "dark");
+        LocalStorage.setItem("Tema", "dark");
     } else {
-        LocalStorage.setItem("tema", "light")
+        LocalStorage.setItem("Tema", "light")
     }
 });
