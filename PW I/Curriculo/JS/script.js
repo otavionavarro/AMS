4f474a34a965a3adc685cabc0f97293f3b42abee
otavionavarro <otavionavarro2011@gmail.com>
2026-05-07 15:30:48 -0300

    Add files via upload

--- PW I/Curriculo/JS/script.js
+++ PW I/Curriculo/JS/script.js
@@ -1,5 +1,6 @@
 const botao = document.getElementById("Tema");
 
+
 // carregar prefêrencia
 if (localStorage.getItem("tema") === "dark") {
     document.body.classList.add("dark");
@@ -15,3 +16,28 @@ botao.addEventListener("click",() => {
         LocalStorage.setItem("Tema", "light")
     }
 });
+
+const updateButtonIcon = () => {
+    if (!botao) return;
+    const darkMode = document.body.classList.contains("dark");
+    botao.textContent = darkMode ? "Mudar para o modo claro" : "Mudar para o modo escuro";
+}
+
+const temaAtual = LocalStorage.getItem("Tema") === "dark" ? "dark" : "light";
+if (temaAtual === "dark") {
+    document.body.classList.add("dark");
+}
+
+updateButtonIcon();
+    if(botao) {
+        botao.addEventListener("click", () => {
+        document.body.classList.toggle("dark");
+    
+    if (document.body.classList.contains("dark")) {
+        localStorage.setItem("Tema", "dark");
+    } else {
+        LocalStorage.setItem("Tema", "light")
+    }
+    updateButtonIcon ();
+});
+}
