d7146ff90e763f9c54a4fdd084df51c77827bae9
otavionavarro <otavionavarro2011@gmail.com>
2026-05-21 13:48:14 -0300

    Add files via upload

--- PW I/Curriculo/JS/script.js
+++ PW I/Curriculo/JS/script.js
@@ -1,43 +1,42 @@
-const botao = document.getElementById("Tema");
+/*-const botao = document.getElementById("Tema");
 
-
-// carregar prefêrencia
-if (localStorage.getItem("tema") === "dark") {
+if (localStorage.getItem("Tema") === "dark"){
     document.body.classList.add("dark");
 }
 
-// alternar tema
-botao.addEventListener("click",() => {
+botao.addEventListener("click", () => { 
     document.body.classList.toggle("dark");
-
     if (document.body.classList.contains("dark")) {
-        LocalStorage.setItem("Tema", "dark");
+        localStorage.setItem("Tema", "dark");
     } else {
-        LocalStorage.setItem("Tema", "light")
+        localStorage.setItem("Tema", "light");
     }
-});
+});*/
+
+const botao= document.getElementById("Tema");
 
 const updateButtonIcon = () => {
     if (!botao) return;
-    const darkMode = document.body.classList.contains("dark");
-    botao.textContent = darkMode ? "Mudar para o modo claro" : "Mudar para o modo escuro";
-}
+    const darkMode = document.body.classList.contains("dark")
+    botao.textContent = darkMode ? "🌓" : "🌗";
+    botao.title = darkMode ? "Mudar para modo claro" : "Mudar para modo escuro";
+};
 
-const temaAtual = LocalStorage.getItem("Tema") === "dark" ? "dark" : "light";
-if (temaAtual === "dark") {
+const TemaAtual = localStorage.getItem("Tema") === "dark" ? "dark" : "light";
+if(TemaAtual === "dark") {
     document.body.classList.add("dark");
 }
 
 updateButtonIcon();
-    if(botao) {
-        botao.addEventListener("click", () => {
-        document.body.classList.toggle("dark");
-    
-    if (document.body.classList.contains("dark")) {
-        localStorage.setItem("Tema", "dark");
-    } else {
-        LocalStorage.setItem("Tema", "light")
-    }
-    updateButtonIcon ();
-});
+
+if (botao) {
+    botao.addEventListener("click", () => { document.body.classList.toggle("dark");
+        if(document.body.classList.contains("dark")) {
+            localStorage.setItem("Tema", "dark");
+        } else {
+            localStorage.setItem("Tema", "light");
+        }
+
+        updateButtonIcon ();
+    });
 }
